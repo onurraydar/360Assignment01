b4d2d4283e89fcecf7f943e669a243fe1a92d8f4
Ray <Ray@onurraydar>
2020-10-05 14:36:49 -0700

    Added main to display output and show functionality, also edited methods

--- Assignment1/src/cse360assignment02/AddingMachine.java
+++ Assignment1/src/cse360assignment02/AddingMachine.java
@@ -2,26 +2,52 @@ package cse360assignment02;
 
 public class AddingMachine {
 	 private int total;
+	 private String print;
 	
 	public AddingMachine () {
 	    total = 0;  // not needed - included for clarity
+	    print = "";
 	  }
 	
 	public int getTotal () {
-	    return 0;
+	    return total;
 	  }
 	  
 	  public void add (int value) {
+		  total = total + value;
+		  if(print.equals("")) {
+			  print = 0 + " + " + value;
+		  }
+		  else {
+			  print = print + " + " + value;
+		  }
 	  }
 
 	  public void subtract (int value) {
+		  total = total - value;
+		  if(print.equals("")) {
+			  print = 0 + " - " + value;
+		  }
+		  else {
+			  print = print + " - " + value;
+		  }
 	  }
 
 	  public String toString () {
-	    return "";
+	    return print;
 	  }
 
 	  public void clear() {
+		  total = 0;
+		  print = "";
 	  }
+	  
+	  public static void main(String[] args) {
+		  AddingMachine myCalculator = new AddingMachine();
+		  myCalculator.add(4);
+		  myCalculator.subtract(2);
+		  myCalculator.add(5);
+		  System.out.println(myCalculator.toString());
+	} 
 
 }
